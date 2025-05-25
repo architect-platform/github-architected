079032f78a6c26c3a6fa56311d144f62bb433f49
Alessandro Mazzoli <developer.alessandro.mazzoli@gmail.com>
2025-05-25 16:01:44 +0200

    fix: removed github package

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
 	kotlin("jvm") version "1.9.25"
-	`maven-publish`
 }
 group = "io.github.architectplatform.plugins"
 version = "1.2.11"
@@ -20,7 +19,7 @@ repositories {
 	repositories {
 		maven {
 			name = "GitHubPackages"
-			url = uri("https://maven.pkg.github.com/architect-platform/github-architected")
+			url = uri("https://maven.pkg.github.com/architect-platform/architect-api")
 			credentials {
 				username = System.getenv("GITHUB_USER") ?: project.findProperty("githubUser") as String? ?: "github-actions"
 				password = System.getenv("REGISTRY_TOKEN") ?: System.getenv("GITHUB_TOKEN") ?: project.findProperty("githubToken") as String?
@@ -33,43 +32,5 @@ dependencies {
 	implementation("io.github.architectplatform:architect-api:1.14.0")
 	implementation("com.fasterxml.jackson.core:jackson-databind:2.19.0") // core Jackson
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.19.0") // Kotlin support
-
 }
-
-publishing {
-	publications {
-		create<MavenPublication>("gpr") {
-			from(components["java"])
-			artifactId = "github-architected"
-			pom {
-				name.set("Gradle Architected")
-				description.set("An Architect plugin for Github")
-				url.set("https://github.com/architect-platform/github-architected")
-				licenses {
-					license {
-						name.set("Apache-2.0")
-						url.set("https://www.apache.org/licenses/MIT-2.0")
-					}
-				}
-				developers {
-					developer {
-						id.set("alemazzo")
-						name.set("Alessandro Mazzoli")
-					}
-				}
-			}
-		}
-	}
-	repositories {
-		maven {
-			name = "GitHubPackages"
-			url = uri("https://maven.pkg.github.com/architect-platform/github-architected")
-			credentials {
-				username = System.getenv("GITHUB_USER") ?: project.findProperty("githubUser") as String? ?: "github-actions"
-				password = System.getenv("REGISTRY_TOKEN") ?: System.getenv("GITHUB_TOKEN") ?: project.findProperty("githubToken") as String?
-			}
-		}
-	}
-}
-
 
