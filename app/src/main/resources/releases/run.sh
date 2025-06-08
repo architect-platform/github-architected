962277dc3759eb74fe67ff9077e1ec0af65fff74
Alessandro Mazzoli <developer.alessandro.mazzoli@gmail.com>
2025-06-08 19:22:46 +0200

    fix: added support for ! breaking changes in commits

--- app/src/main/resources/releases/run.sh
+++ app/src/main/resources/releases/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 npm init -y
-npm install --save-dev semantic-release @semantic-release/changelog @semantic-release/commit-analyzer @semantic-release/release-notes-generator @semantic-release/github @semantic-release/git @semantic-release/exec
+npm install --save-dev semantic-release @semantic-release/changelog @semantic-release/commit-analyzer @semantic-release/release-notes-generator @semantic-release/github @semantic-release/git @semantic-release/exec semantic-release-preconfigured-conventional-commits
 touch CHANGELOG.md
 npm ci
 npx semantic-release
